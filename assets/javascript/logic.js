35348e6c5992d59f4d7977ff091cc9b137127d96
Alexperkky <alex.perkinson5@gmail.com>
2018-09-24 13:41:38 -0700

    Some more changes

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -80,7 +80,7 @@ var game = {
     },
     start: function(){
         timer = setInterval(game.countdown,1000);
-        $('#subwrapper').prepend('<h2>Time Remaining: <span id = "counter">120</span> Seconds</h2>')
+        $('#subwrapper').prepend('<h2>Time Remaining: <span id = "counter">60</span> Seconds</h2>')
         $('#start').remove();
         for(var i = 0; i < questions.length; i++){
             $('#subwrapper').append('<h2>' +questions[i].question + '</h2>');
@@ -168,6 +168,16 @@ var game = {
             } else {
                 game.incorrect++;
             }
-        })
+        });
+    this.result();
+    },
+
+    result: function(){
+        clearInterval(timer);
+        $('#subwrapper h2').remove();
+        $('#subwrapper').html("<h2>All Done!</h2>");
+        $('#subwrapper').append("<h3>Correct Answers:" +this.correct+"</h3>");
+        $('#subwrapper').append("<h3>Incorrect Answers:" +this.incorrect+"</h3>");
+        $('#subwrapper').append("<h3>Unanswered: " +(questions.length - (this.incorrect + this.correct))+"</h3>");
     }
 }
